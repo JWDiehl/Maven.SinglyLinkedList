8e1cfa015e3c11737d942ad8599f229a88af87c2
Jon <jon@zipcodersmbp99.myfiosgateway.com>
2024-07-14 16:37:38 -0400

    First Commit

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,25 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    //
+
 }
