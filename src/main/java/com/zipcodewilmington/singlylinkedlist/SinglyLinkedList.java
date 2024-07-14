23c057c51e38bbfa4fb5c839fb5db25d042663c1
Jon <jon@zipcodersmbp99.myfiosgateway.com>
2024-07-14 18:12:33 -0400

    7 out of 8 test passed, debugging to get last test to pass

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,26 +3,184 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList <E extends Comparable<E>> {
 
-    public static class Node {
-        int data;
+    //Node inner class to represent each element in the linked list
+    public class Node {
+        E data;
         Node next;
 
-        public Node(int data) {
+        Node(E data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    private Node head;
+    //Head of the list
+    public Node head;
+
+    //Size of the list
     private int size;
 
+    //Constructor
     public SinglyLinkedList() {
-        this.head = null;
-        this.size = 0;
+        head = null;
+        size = 0;
     }
 
-    //
+    //Methods --
 
+    //Add an element to end of List
+    public void add(E element) {
+        Node newNode = new Node(element);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if (index ==0) {
+            head = head.next;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        size--;
+    }
+
+    //Method to check if the lists contains specific element
+    public boolean contains(E element) {
+        Node current = head;
+        while(current != null) {
+            if(current.data.equals(element)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    //Method to find the index of a spec element
+    public int find(E element) {
+        Node current = head;
+        int index = 0;
+        while (current != null) {
+            if(current.data.equals(element)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    //Size method
+    public int size() {
+        return size;
+    }
+
+    //Method to create a COPY
+    public SinglyLinkedList<E> copy() {
+        SinglyLinkedList<E> newList = new SinglyLinkedList<>();
+        Node current = head;
+        while(current != null) {
+            newList.add(current.data);
+            current = current.next;
+        }
+        return newList;
+    }
+
+    private void add(int data) {
+    }
+
+    //Method to get the element at a spec index
+    public E get(int index) {
+        if (index < 0 || index>= size) {
+            throw new IndexOutOfBoundsException("Index" + index + ", Size: " + size);
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    //Method to sort the list - bubble sort algo
+    public SinglyLinkedList<E> sort() {
+        if (size > 1) {
+            boolean swapped;
+            do {
+                swapped = false;
+                Node current = head;
+                Node previous = null;
+                while (current.next != null) {
+                    if (current.data.compareTo(current.next.data) > 0) {
+                        //Swap
+                        Node temp = current.next;
+                        current.next = temp.next;
+                        temp.next = current;
+                        if (previous == null) {
+                            head = temp;
+                        } else {
+                            previous.next = temp;
+                        }
+                        previous = temp;
+                        swapped = true;
+                    } else {
+                        previous = current;
+                        current = current.next;
+                    }
+                }
+            } while (swapped);
+        }
+        return null;
+    }
+
+    //Method to reverse linked list
+    public void reverse() {
+        if (head == null || head.next == null) {
+            return;
+        }
+        Node previous = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
+
+    //Method to slice the list and reutrn copy of elements
+    public SinglyLinkedList<E> slice(int start, int end) {
+        if (start < 0 || end > size || start >= end) {
+            throw new IllegalArgumentException("Invalid slice indices");
+        }
+        SinglyLinkedList<E> slicedList = new SinglyLinkedList<>();
+        Node current = head;
+        int index = 0;
+        while (current != null) {
+            if (index >= start && index < end) {
+                slicedList.add(current.data);
+            }
+            current = current.next;
+            index++;
+        }
+        return slicedList;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,10 +1,138 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by leon on 1/10/18.
  */
+
 public class SinglyLinkedListTest {
 
+    private SinglyLinkedList<Integer> list;
 
+    @Before
+    public void setUp() {
+        list = new SinglyLinkedList<>();
+    }
 
+    //add -- add an element to the list
+    @Test
+    public void testAddAndGet() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assertEquals(3, list.size());
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(2), list.get(1));
+        assertEquals(Integer.valueOf(3), list.get(2));
+    }
+
+    //remove -- remove an element (specified by numeric index) from the list
+    @Test
+    public void testRemove() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.remove(1);
+        assertEquals(2, list.size());
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(3), list.get(1));
+    }
+
+    //contains -- returns true if the element is in the list, false otherwise
+    @Test
+    public void testContains() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assertTrue(list.contains(1));
+        assertFalse(list.contains(88));
+    }
+
+    //find -- returns the element's index if it is in the list, -1 otherwise
+    @Test
+    public void testFind() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assertEquals(1, list.find(2));
+        assertEquals(88, list.find(7));
+    }
+
+    //size -- returns the current size of the list
+    @Test
+    public void testSize() {
+        assertEquals(0, list.size());
+
+        list.add(1);
+        assertEquals(1, list.size());
+
+        list.add(2);
+        list.add(3);
+        assertEquals(3, list.size());
+
+        list.remove(0);
+        assertEquals(2, list.size());
+
+        list.remove(1);
+        assertEquals(1, list.size());
+    }
+
+    //get -- returns the element at the specified index
+    @Test
+    public void testGet() {
+        list.add(88);
+        list.add(89);
+        list.add(90);
+
+        assertEquals(Integer.valueOf(88), list.get(0));
+        assertEquals(Integer.valueOf(89), list.get(1));
+        assertEquals(Integer.valueOf(90), list.get(2));
+    }
+
+    //copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+    @Test
+    public void testCopy() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        SinglyLinkedList<Integer> newList = list.copy();
+
+        assertEquals(list.size(), newList.size());
+        assertEquals(list.get(0), newList.get(0));
+        assertEquals(list.get(1), newList.get(1));
+        assertEquals(list.get(2), newList.get(2));
+
+        //Ensure they are seperate instances
+        assertNotSame(list, newList);
+        assertNotSame(list.head, newList.head);
+
+    }
+
+    //sort -- sorts the list using your algorithm of choice.
+    // You must perform the sorting yourself (no fair using someone else's library)
+    @Test
+    public void testSort() {
+        list.add(88);
+        list.add(21);
+        list.add(55);
+        list.add(2);
+        list.add(91);
+
+        list.sort();
+
+        assertEquals(Integer.valueOf(2), list.get(0));
+        assertEquals(Integer.valueOf(21), list.get(1));
+        assertEquals(Integer.valueOf(55), list.get(2));
+        assertEquals(Integer.valueOf(88), list.get(3));
+        assertEquals(Integer.valueOf(91), list.get(4));
+    }
 }
